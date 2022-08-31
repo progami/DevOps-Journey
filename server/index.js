e8f7c1e9e3a8fce0b79981835159e926d67b51de
Jarrar <jarraramjad@gmail.com>
2022-08-31 10:50:30 +0500

    115. Completed express app server side

--- server/index.js
+++ server/index.js
@@ -48,5 +48,32 @@ app.get('/', (req, res) => {
     res.send('Hi')
 });
 
-app.get('/values/all', async ())
+app.get('/values/all', async (req, res) => {
+    const values = await pgClient.query('SELECT * from values')
 
+    res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+    redisClient.hgetall('values', (err, values) => {
+    
+    });
+});
+
+app.post('/values', async(req, res) => {
+    const index = req.body.index;
+
+    if (parseInt(index) > 40){
+        return res.status(422).send('Index too high');
+    }
+
+    redisClient.hset('values', index, 'Nothing yet');
+    redisPublisher.publish('insert', index);
+    pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+    res.send({working: true});
+});
+
+app.listen(5000, err => {
+    console.log('Listening');
+});
